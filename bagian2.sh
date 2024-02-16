f8f8642be09676e59e0a37aedcd1671730c06ee2
smahud <35734598+smahud@users.noreply.github.com>
2024-02-16 16:40:48 +0700

    Update bagian2.sh

--- bagian2.sh
+++ bagian2.sh
@@ -4,7 +4,6 @@ source /root/speedtest/common_functions1.sh
 if [ -e "/root/OoklaServer" ]; then
 echo "OoklaServer sudah terinstal. Melewati ke Bagian II."
 print_hash 30
-
 else
 echo "OoklaServer tidak ditemukan. Menjalankan Perintah Instalasi OoklaServer"
 /root/speedtest/serverinstall.sh 
@@ -13,6 +12,4 @@ cp /root/speedtest/ooklaserver.sh /root/ooklaserver.sh
 echo "OoklaServer berhasil di install"
 echo "Melanjutkan Bagian II"
 print_hash 30
-
-
 fi
