002c1c38a73242597b1b86b4df874fd00c959a7f
smahud <35734598+smahud@users.noreply.github.com>
2024-02-16 14:38:00 +0700

    bagian1.sh

---
+++ bagian1.sh
@@ -0,0 +1,20 @@
+# Cek apakah file data.ini sudah ada
+echo "Melakukan input dari file data.ini"
+if [ -e "/root/data.ini" ]; then
+    echo "File data.ini ditemukan, melanjutkan perintah..."
+	print_hash 30
+else
+    echo "File data.ini tidak ditemukan. Skrip dihentikan."
+	print_hash 100
+    exit 1
+fi
+# Sumberkan data.ini
+source /root/data.ini
+sudo mkdir -p /etc/letsencrypt
+rm /etc/letsencrypt/dnscloudflare.ini
+sudo tee /etc/letsencrypt/dnscloudflare.ini > /dev/null <<END
+dns_cloudflare_email = $EmailCloudFlare
+dns_cloudflare_api_key = $APICloudFlare
+END
+sudo chmod 0600 /etc/letsencrypt/dnscloudflare.ini
+##############################################################
