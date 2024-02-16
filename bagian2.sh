21256ada2d721b718d8a250bac973b7c0e90e631
smahud <35734598+smahud@users.noreply.github.com>
2024-02-16 14:46:20 +0700

    bagian2.sh

--- bagian2.sh
+++ bagian2.sh
@@ -7,29 +7,6 @@ print_hash 30
 else
 echo "OoklaServer tidak ditemukan. Menjalankan Perintah Instalasi OoklaServer"
 
-# Mendeteksi distribusi Linux yang digunakan
-if [ -f /etc/debian_version ]; then
-    # Debian/Ubuntu
-    sudo apt update
-    sudo apt install git -y
-elif [ -f /etc/redhat-release ]; then
-    # RHL/CentOS
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
-        if [ "$ID" == "rhel" ] || [ "$ID" == "centos" ]; then
-            yum update -y
-			sudo yum install git -y
-        fi
-    fi
-elif [ -f /etc/SuSE-release ]; then
-    # OpenSUSE
-	echo "Terdeteksi openSUSE"
-    sudo zypper install git -y
-else
-    echo "Distribusi Linux tidak didukung atau tidak dapat dideteksi."
-    exit 1
-fi
-
 git clone https://github.com/smahud/speedtest.git 
 chmod a+x /root/speedtest/*.sh 
 /root/speedtest/serverinstall.sh 
