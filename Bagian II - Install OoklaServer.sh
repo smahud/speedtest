84ae9936f8f3c75b8e611d8c92acd4bc9b483e4b
smahud <35734598+smahud@users.noreply.github.com>
2024-03-23 13:30:16 +0700

    Update Bagian II - Install OoklaServer.sh

--- Bagian II - Install OoklaServer.sh
+++ Bagian II - Install OoklaServer.sh
@@ -6,7 +6,23 @@ echo "OoklaServer sudah terinstal. Melewati ke Bagian II."
 print_hash 30
 else
 echo "OoklaServer tidak ditemukan. Menjalankan Perintah Instalasi OoklaServer"
-/root/speedtest/serverinstall.sh 
+#/root/speedtest/serverinstall.sh (lawas, tidak dipakai)
+
+echo "Membuat file OoklaServer.properties "
+cat <<EOF | sudo tee /root/OoklaServer.properties > /dev/null
+OoklaServer.tcpPorts = 5060,8080
+OoklaServer.udpPorts = 5060,8080
+OoklaServer.useIPv6 = true
+OoklaServer.allowedDomains = *.ookla.com, *.speedtest.net
+OoklaServer.enableAutoUpdate = true
+OoklaServer.ssl.useLetsEncrypt = true
+logging.loggers.app.name = Application
+logging.loggers.app.channel.class = ConsoleChannel
+logging.loggers.app.channel.pattern = %Y-%m-%d %H:%M:%S [%P - %I] [%p] %t
+logging.loggers.app.level = information
+EOF
+
+
 cd 
 cp /root/speedtest/ooklaserver.sh /root/ooklaserver.sh
 echo "OoklaServer berhasil di install"
