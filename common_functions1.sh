2c8980cd85d09ace43f7cc54fd0288d1502d09ac
smahud <35734598+smahud@users.noreply.github.com>
2025-03-20 23:49:46 +0700

    Update common_functions1.sh

--- common_functions1.sh
+++ common_functions1.sh
@@ -1,8 +1,10 @@
 print_hash() {
-    local count=$1
-    for ((i=1; i<=count; i++)); do
+    count="$1"  # Tidak menggunakan 'local' karena tidak semua shell mendukungnya
+    i=1
+    while [ "$i" -le "$count" ]; do
         printf "#"
         sleep 0.05
+        i=$((i + 1))
     done
     echo  # Baris baru setelah mencetak karakter #
 }
