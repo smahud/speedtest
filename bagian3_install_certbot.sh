3b730164888cfc0515688d37d130e1c65b54a327
smahud <35734598+smahud@users.noreply.github.com>
2025-03-20 23:41:25 +0700

    Update bagian3_install_certbot.sh

--- bagian3_install_certbot.sh
+++ bagian3_install_certbot.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
-
-# Sumber fungsi umum (pastikan file ini ada)
-source /root/speedtest/common_functions1.sh
-
+# Bagian III
 # Fungsi untuk memeriksa apakah suatu perintah ada
+source /root/speedtest/common_functions1.sh
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
@@ -20,24 +17,25 @@ if certbot_installed; then
 else
     # Fungsi untuk menjalankan instalasi Certbot untuk Ubuntu/Debian
     install_certbot_ubuntu() {
-        apt update
-        apt install -y snapd
+        apt install snapd -y
         snap install core
         snap refresh core
         snap install --classic certbot
         ln -s /snap/bin/certbot /usr/bin/certbot
-        apt install -y certbot
+        apt install certbot -y
         snap set certbot trust-plugin-with-root=ok
         snap install certbot-dns-cloudflare
-        apt install -y python3 python3-pip python3-certbot-dns-cloudflare
-        pip install cloudflare==2.3.1 --break-system-packages
-        pip install --upgrade cloudflare --break-system-packages
+        apt install python3 -y
+        apt install python3-pip -y
+        apt install python3-certbot-dns-cloudflare -y
+        pip install cloudflare==2.3.1
+        pip install --upgrade cloudflare
     }
 
     # Fungsi untuk menjalankan instalasi Certbot untuk CentOS/RHEL
     install_certbot_centos() {
-        yum install -y epel-release
-        yum install -y snapd
+        yum install epel-release -y
+        yum install snapd -y
         systemctl enable --now snapd.socket
         ln -s /var/lib/snapd/snap /snap
         sleep 1
@@ -47,51 +45,54 @@ else
         snap set certbot trust-plugin-with-root=ok
         snap install certbot-dns-cloudflare
         ln -s /snap/bin/certbot /usr/bin/certbot
-        yum install -y certbot python3 python3-pip gcc libffi-devel python3-devel openssl-devel
+        yum install certbot -y
+        yum install python3 -y
+        yum install python3-pip -y
+        pip install certbot-dns-cloudflare --root-user-action=ignore
+        yum -y install gcc libffi-devel python3-devel openssl-devel
         pip install --upgrade pip
-        pip install --upgrade certbot certbot-dns-cloudflare cryptography
+        pip install --upgrade certbot
+        pip install cryptography
         firewall-cmd --zone=dmz --add-port=8080/tcp --permanent
         firewall-cmd --reload
     }
 
     # Fungsi untuk menjalankan instalasi Certbot untuk openSUSE
     install_certbot_opensuse() {
-        zypper install -y certbot
+        zypper install certbot
         if ! command_exists certbot-dns-cloudflare; then
-            zypper install -y python3-certbot-dns-cloudflare
+            zypper install python3-certbot-dns-cloudflare
         fi
     }
-
+    
     # Fungsi untuk menjalankan instalasi Certbot untuk Alpine
     install_certbot_alpine() {
         apk update
-        apk add certbot py3-certbot-dns-cloudflare
+        apk add certbot
+        apk add py3-certbot-dns-cloudflare
     }
 
     # Tentukan OS dan jalankan fungsi yang sesuai
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
-        case "$ID" in
-            ubuntu|debian)
-                install_certbot_ubuntu
-                ;;
-            centos|rhel|fedora)
-                install_certbot_centos
-                ;;
-            opensuse|suse)
-                install_certbot_opensuse
-                ;;
-            alpine)
-                install_certbot_alpine
-                ;;
-            *)
-                echo "Distribusi Linux tidak didukung."
-                print_hash 100
-                exit 1
-                ;;
-        esac
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        if command_exists zypper; then
+            install_certbot_opensuse
+            print_hash 30
+        elif command_exists apt; then
+            install_certbot_ubuntu
+            print_hash 30
+        elif command_exists yum; then
+            install_certbot_centos
+            print_hash 30
+        elif command_exists apk; then
+            install_certbot_alpine
+            print_hash 30
+        else
+            echo "Distribusi Linux tidak didukung."
+            print_hash 100
+            exit 1
+        fi
     else
-        echo "Sistem operasi tidak dikenali."
+        echo "Sistem operasi tidak didukung."
         print_hash 100
         exit 1
     fi
