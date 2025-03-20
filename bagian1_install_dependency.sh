df0ab8e75bd1d19c0b2532815f99c718aac60093
smahud <35734598+smahud@users.noreply.github.com>
2025-03-21 01:18:21 +0700

    Update bagian1_install_dependency.sh

--- bagian1_install_dependency.sh
+++ bagian1_install_dependency.sh
@@ -11,6 +11,11 @@ else
 	print_hash 100
     exit 1
 fi
+# Install Speedtest For Test
+wget -O /tmp/speedtest.tgz https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz
+tar -xvzf /tmp/speedtest.tgz -C /usr/local/bin --strip-components=1 speedtest
+chmod a+x /usr/local/bin/speedtest
+
 # Sumberkan data.ini
 mkdir -p /etc/letsencrypt
 rm /etc/letsencrypt/dnscloudflare.ini
